3e49a383fb077b0c4e9f38657108e4fc26cdb72e
Álvaro Enol Alonso Ortega <alvaroea@ucm.es>
2025-10-15 12:43:42 +0200

    Añadido la busqueda de ingredientes

--- sbc/cli.py
+++ sbc/cli.py
@@ -1,1 +1,12 @@
-print("Hello World")
+from pathlib import Path
+
+fichero = Path("fdi-sbc-2503/kb/ingredientes.txt")
+
+lineas = fichero.read_text(encoding="utf8").splitlines()
+ingredientes = [linea.lower() for linea in lineas if linea.strip()]
+
+
+ingrediente_buscar = "tomate"
+
+if ingrediente_buscar.lower() in ingredientes:
+    print(f"'{ingrediente_buscar}' está en la lista.")
